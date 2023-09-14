0166aed73a2b1f1e1a400404e8dfbf3a0238ce99
Mitch McCoard <mitch.mccoard@gmail.com>
2023-09-14 15:00:56 -0600

    displays radnom word, checks if word is in list

--- Wordle.py
+++ Wordle.py
@@ -11,12 +11,36 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
-
+    
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        s = s.lower()
+        
+        if s == random_word:
+            gw.show_message("Congratulations! You guessed the word correctly!")
+        else:
+            if s in FIVE_LETTER_WORDS:
+                gw.show_message("Smart! Keep at it")
+                current_row = gw.get_current_row()
+                if current_row < N_ROWS - 1:
+                    gw.set_current_row(current_row + 1)
+            else:
+                gw.show_message("Nope that is not a word")
+                current_row = gw.get_current_row()
+                gw.set_current_row(current_row)
 
     gw = WordleGWindow()
+
+    random_word = random.choice(FIVE_LETTER_WORDS)
+
+    for col in range(N_COLS):
+        gw.set_square_letter(0,col, random_word[col])
     gw.add_enter_listener(enter_action)
+
+    # def enter_action(s):
+    #     gw.show_message("You have to implement this method.")
+
+    # gw = WordleGWindow()
+    # gw.add_enter_listener(enter_action)
 
 # Startup code
 
