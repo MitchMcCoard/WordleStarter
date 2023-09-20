087bc92b8bec444f0a9f02b84485812c8fed37d8
SkiMaskSKE <95718642+SkiMaskSKE@users.noreply.github.com>
2023-09-20 15:39:06 -0600

    Created new file with old code incase

--- Wordle.py
+++ Wordle.py
@@ -1,88 +1,82 @@
 # File: Wordle.py
 
-"""
-This module is the starter file for the Wordle assignment.
-BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
-"""
-
 import random
-
 
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
-Solution = random.choice(FIVE_LETTER_WORDS).upper()
+MAX_GUESSES = 6  # Maximum number of guesses
 
 def wordle():
-    #gw = WordleGWindow()
-    #i = 0
-    #for i, letter in enumerate(Solution):
-    #    gw.set_square_letter((N_ROWS - N_ROWS), (N_COLS - (N_COLS - i)), Solution[i])
-    #    i += 1
 
     def enter_action(s):
         s = s.lower()
         
         if s == random_word:
-            gw.show_message("Congratulations! You guessed the word correctly!")
+            gw.show_message("Congratulations! You guessed the word correctly!\nPress Esc to exit.")
+            for col in range(N_COLS):
+                current_row = gw.get_current_row()
+                gw.set_square_color(current_row, col, CORRECT_COLOR)
+
+                # Disable further input and end the game
+                gw._root.unbind("<Key>")
         else:
             if s in FIVE_LETTER_WORDS:
-                gw.show_message("Smart! Keep at it")
                 current_row = gw.get_current_row()
-                if current_row < N_ROWS - 1:
-                    gw.set_current_row(current_row + 1)
+                
+                if current_row < N_ROWS:
+                    # Get the randomly chosen word
+                    target_word = list(random_word)
+
+                    # Initialize lists to track correctness
+                    correct_letters = []
+                    present_letters = []
+
+                    # Check each letter in the guess
+                    for col in range(N_COLS):
+                        guess_letter = s[col]
+                        target_letter = target_word[col]
+
+                        if guess_letter == target_letter:
+                            correct_letters.append(col)
+                            target_word[col] = ''  # Mark as used
+
+                    # Now, check for present but misplaced letters
+                    for col in range(N_COLS):
+                        guess_letter = s[col]
+                        if guess_letter and guess_letter in target_word:
+                            present_letters.append(col)
+                            target_word[target_word.index(guess_letter)] = ''  # Mark as used
+
+                    # Update square colors based on correctness
+                    for col in range(N_COLS):
+                        if col in correct_letters:
+                            gw.set_square_color(current_row, col, CORRECT_COLOR)
+                        elif col in present_letters:
+                            gw.set_square_color(current_row, col, PRESENT_COLOR)
+                        else:
+                            gw.set_square_color(current_row, col, MISSING_COLOR)
+                    # Check if the player is out of guesses
+                    if current_row == MAX_GUESSES - 1:
+                            gw.show_message(f"Out of guesses! The word was: {random_word} \nPress Esc to exit.")
+                    # Advance to the next row
+                    if current_row < N_ROWS - 1:
+                        gw.set_current_row(current_row + 1)
             else:
-                gw.show_message("Nope that is not a word")
+                gw.show_message("Nope, that is not a word")
                 current_row = gw.get_current_row()
                 gw.set_current_row(current_row)
-
     gw = WordleGWindow()
 
     random_word = random.choice(FIVE_LETTER_WORDS)
-
-    for col in range(N_COLS):
-        gw.set_square_letter(0,col, random_word[col])
-
-    ##########################################################    
-    i = 0
-    
-    current_row = gw.get_current_row()
-    typed_word = "".join([gw.get_square_letter(current_row, i) for i in range(N_COLS)])
-
-    # Initialize lists to keep track of correctly guessed letters and their positions
-    correct_letters = []
-    correct_positions = []
-    
-    # Check each letter in the typed word against the random word
-    
-    for i in range(N_COLS):
-        typed_letter = typed_word[i]
-        random_letter = Solution[i]
-        
-        if typed_letter == random_letter:
-            # Letter is correct and in the correct position
-            correct_letters.append(typed_letter)
-            correct_positions.append(i)
-        elif typed_letter in Solution and typed_word.count(typed_letter) <= Solution.count(typed_letter):
-            # Letter is correct but in the wrong position
-            correct_letters.append(typed_letter)
-    
-    # Check if the player has correctly guessed all five letters
-    if typed_word == Solution:
-        gw.show_message("Congratulations! You guessed the word correctly!")
-    else:
-        # Check if the typed word is a legitimate English word
-        if typed_word.lower() in FIVE_LETTER_WORDS:
-            # Move to the next row
-            current_row = gw.get_current_row()
-            if current_row < N_ROWS - 1:
-                gw.set_current_row(current_row + 1)
-        else:
-            gw.show_message("Not in word list")
-            gw.set_current_row(current_row)
-    ####################################################
-
+    #Show Answer in first row
+    # for col in range(N_COLS):
+    #     gw.set_square_letter(0, col, random_word[col])
     gw.add_enter_listener(enter_action)
+
+     # Allow the player to exit the game by pressing Esc
+    gw._root.bind("<Escape>", lambda event: gw._root.destroy())
 
 # Startup code
 
